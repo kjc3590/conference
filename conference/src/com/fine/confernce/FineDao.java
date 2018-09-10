fd7178d21581067921db413070df99d710afb3e0
JongChan <JongChan@Jong>
2018-09-10 11:43:13 +0900

    db 세팅 및 java 버젼 변경

---
+++ conference/src/com/fine/confernce/FineDao.java
@@ -0,0 +1,109 @@
+package com.fine.confernce;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class FineDao {
+
+	private static FineDao instance = new FineDao();
+
+	public static FineDao getInstance() {
+		return instance;
+	}
+
+	private Connection getConnection() throws Exception {
+		Context initCtx = new InitialContext();
+		DataSource ds = 
+				//				(DataSource)initCtx.lookup("java:comp/env/jdbc/fineinsight");
+				(DataSource)initCtx.lookup("java:comp/env/jdbc/mysql");
+
+		return ds.getConnection();
+	}
+
+	private void executeClose(ResultSet rs, PreparedStatement pstmt, Connection conn){
+		if (rs != null) {
+			try {
+				rs.close();
+			}
+			catch (SQLException localSQLException) {
+
+			}
+		}
+		if (pstmt != null) {
+			try {
+				pstmt.close();
+			}
+			catch (SQLException localSQLException1) {
+			}
+		}
+		if (conn != null) {
+			try {
+				conn.close();
+			}
+			catch (SQLException localSQLException2) {
+			}
+		}
+	}
+
+	public int confe_confirm(String phone , String name) throws Exception  {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "";
+		ResultSet rs = null;
+		int count = 0;
+		try {
+			conn = getConnection();
+
+			sql = "select count(*) from confe_info where confe_phone = ? and confe_name = ?";
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, phone);
+			pstmt.setString(2, name);
+			rs = pstmt.executeQuery();
+
+			count = rs.getInt(1);
+
+		}
+		catch (Exception e)  {
+			throw new Exception(e);
+		}finally {
+			executeClose(rs, pstmt, conn);
+		}
+
+		return count;
+	}
+
+	public void confe_insert(FineVo vo) throws Exception {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "";
+		try {
+			conn = getConnection();
+
+			sql = "insert into confe_info values (?,?,?,?,?) ";
+
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, vo.getConfe_affiliation());
+			pstmt.setString(2, vo.getConfe_position());
+			pstmt.setString(3, vo.getConfe_email());
+			pstmt.setString(4, vo.getConfe_phone());
+			pstmt.setString(5, vo.getConfe_name());
+			pstmt.executeUpdate();
+		}
+		catch (Exception e) {
+			throw new Exception(e);
+		}
+		finally {
+			executeClose(null, pstmt, conn);
+		}
+	}
+
+}
---
+++ conference/src/com/fine/confernce/FineVo.java
@@ -0,0 +1,50 @@
+package com.fine.confernce;
+
+public class FineVo {
+	
+	private int confe_id;
+	private String confe_name;
+	private String confe_affiliation;
+	private String confe_position;
+	private String confe_email;
+	private String confe_phone;
+	
+	public int getConfe_id() {
+		return confe_id;
+	}
+	public void setConfe_id(int confe_id) {
+		this.confe_id = confe_id;
+	}
+	public String getConfe_name() {
+		return confe_name;
+	}
+	public void setConfe_name(String confe_name) {
+		this.confe_name = confe_name;
+	}
+	public String getConfe_affiliation() {
+		return confe_affiliation;
+	}
+	public void setConfe_affiliation(String confe_affiliation) {
+		this.confe_affiliation = confe_affiliation;
+	}
+	public String getConfe_position() {
+		return confe_position;
+	}
+	public void setConfe_position(String confe_position) {
+		this.confe_position = confe_position;
+	}
+	public String getConfe_email() {
+		return confe_email;
+	}
+	public void setConfe_email(String confe_email) {
+		this.confe_email = confe_email;
+	}
+	public String getConfe_phone() {
+		return confe_phone;
+	}
+	public void setConfe_phone(String confe_phone) {
+		this.confe_phone = confe_phone;
+	}
+	
+
+}
